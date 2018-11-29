44f951f27e224e9bdcbf1c13b05cfa1a4b2df998
Hanna Yang <hyang6@stuy.edu>
2018-11-29 10:24:14 -0500

    oops

---
+++ text_parse.h
@@ -0,0 +1,33 @@
+#ifndef text
+#define text
+
+
+//                      DEALING W TEXT
+//
+
+// inputs: char* line, char* delim
+// returns: (unsigned int) number of tokens separated by @delim
+// counts number of times @delim appears in @line, and adds 1.
+
+unsigned int num_tokens(char* line, char* delim);
+
+
+// inputs: char* line, char* delim
+// returns: array of lineings where each entry is a token (from @str) seperated by @delim
+// seperates @line into tokens by @delim, and returns a pointer to an array contining them
+
+char** parse_line(char* line, char* delim, int num_tokens);
+
+// inputs: char** args
+// returns: char** that has everything in args, but with the whitespace trimmed off on either end
+// trims off whitespace: \t, spaces, etc and other random stuff
+
+void trim(char** args);
+
+// inputs: char c
+// returns: 1 if c is some sort of white space charactere, 0 otherwise
+// checks to see if c is '\t' or ' ' 
+
+char is_space(char c);
+
+#endif
